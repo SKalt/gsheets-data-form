94aac4a8e5539cd2d5519ca5baba726b1b3e6b73
SKalt <kalt.steven@gmail.com>
2017-09-11 18:50:28 -0400

    ui in progress, moving to rollup

--- automator/src/index.js
+++ automator/src/index.js
@@ -1,12 +1,62 @@
-import {createStore, combineReducers} from 'redux';
+// import {createStore, combineReducers} from 'redux';
+import {formatUrl} from '../../src/url-formatter.js';
 
-const linkToId = (link) => link
-  .replace(/.*\/d\//, '')
+const linkToId = (link) => link.replace(/.*\/d\//, '')
   .replace(/\/edit.+/, '');
 
+const get = {
+  _:(id)=>document.getElementById(id),
+  all: (selector)=>document.querySelectorAll(selector),
+  valueOf: (id)=>get._(id).value,
+  key: ()=>get.valueOf('api-key-form'),
+  sheetId: ()=>linkToId(get.valueOf('ID')),
+  link: ()=>get.valueOf('fetch-link'),
+  range: ()=>get.valueOf('range')
+}
+
+const test = {
+  link: ()=>{
+    fetch(get.link()).then(response => {
+      if (response.ok){
+        return response.json();
+      }
+      return response.text();
+    })
+      .then(content => document.getElementById('test').textContent = content);
+  }
+}
+
+const makeLink = () => {
+  let params = {
+    key: get.key(),
+    sheetId: get.sheetId(),
+    range: get.range()
+  };
+  let url = formatUrl(params);
+  document.getElementById('fetch-link').value = url;
+}
+
+const copyLink = () => {
+  document.getElementById('fetch-link').select();
+  document.execCommand('copy');
+}
+const toggle = (el) =>{
+  el.css.display = el.css.display ? '' : 'none';
+}
 // poll for window closing
 function pollWindow(url, cb){
   let tab = window.open(url);
   const checkOpen = () => setTimeout(()=> tab.closed ? cb() : checkOpen(), 500);
   checkOpen();
 }
+
+window.addEventListener('load', function() {
+  get._('api-key-link').addEventListener('click', e => {
+    get._('api-key-input').focus();
+  });
+  get('make-link').addEventListener('click', makeLink);
+  get('test-link').addEventListener('click', test.link);
+  get.all('section > h4').forEach(el =>{
+    el.addEventListener('click', e => toggle(el.nexElementSibling));
+  });
+});
--- src/url-formatter.js
+++ src/url-formatter.js
@@ -1,11 +1,12 @@
-const stringify = require('querystring').stringify;
+import {stringify} from 'querystring';
 /**
  * dateTimeRenderOption majorDimension valueRenderOption prettyPrint upload_protocol uploadType $.xgafv
 callback alt pp
  * @param  {[type]} params [description]
  * @return {[type]}        [description]
+ *
  */
-module.exports = function formatUrl(params){
+export function formatUrl(params){
   let {sheetId, range} = params;
   delete params.sheetId;
   delete params.range;
